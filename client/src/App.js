f0c22d2db841394dbb3a936b56ea0bbdaadbca76
kam-stand <kamrul.hassan@stonybrook.edu>
2023-12-20 22:36:18 -0500

    Added client and server folder. Created welcome component

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Welcome from './component/welcome/welcome';
+
+function App() {
+  return (
+    <>
+    <Welcome/>
+    </>
+  );
+}
+
+export default App;
---
+++ client/src/component/welcome/welcome.js
@@ -0,0 +1,11 @@
+export default function Welcome() {
+    return (
+        <>
+            <ol>
+                <li>Welcome</li>
+                <li>Login page</li>
+                <li>Signup page</li>
+            </ol>
+        </>
+    )
+}
