885b4dab0d79984893f7fe1eb62ad2d2bb23cc76
kam-stand <kamrul.hassan@stonybrook.edu>
2023-12-22 22:47:35 -0500

    created welcome page and layout. login and signup logic finished

--- client/src/component/login/login.js
+++ client/src/component/login/login.js
@@ -1,42 +1,15 @@
-import Signup from "../signup/signup"
 import React, { useState } from 'react';
+import LoginForm from './loginform';
 
 export default function Login() {
-    const[registered, notregistered] = useState(false)
-    const login = () =>{
-        return(
-            <form className="login_page">
-            <div>
-                <label>Username</label>
-                <input type="text" placeholder="Enter your usernmae"></input>
-            </div>
-            <div>
-                <label>Password</label>
-                <input type="text" placeholder="Enter your password"></input>
-            </div>
-            <div>
-                <button>Login</button>
-            </div>
-            <p>Not registered? No problem signup</p>
-            <button>register</button>
-        </form>
-
-        )
+    const [login, loginform] = useState(false)
+    const handleLogin = () =>{
+       loginform(!login)
     }
-    const Render = () =>{
-        if(!registered){
-            return(
-                <Signup/>
-            )
-        }
-        else{
-            return(
-                login()
-            )
-        }
-    }
-
-    return (
-        Render()
-    )
+  return(
+    <>
+    <button onClick={handleLogin}>Login</button>
+    {login ?<LoginForm/> : null}
+    </>
+  )
 }
--- client/src/component/welcome/welcome.js
+++ client/src/component/welcome/welcome.js
@@ -1,23 +1,33 @@
 import Login from "../login/login";
+import Signup from "../signup/signup";
 
 export default function Welcome() {
     return (
         <>
-          <div className="welcome_page">
-            <h1>Todo-List</h1>
-            <p>Here are some of the features</p>
-            <div>
-                <ol>
-                    <li>Tasks</li>
-                    <li>Create notes</li>
-                    <li>Event planning</li>
-                    <li>Visualize productivity</li>
-                </ol>
+        <div className="welcome">
+            <div className="title">
+            <h1>ProNotion</h1>
             </div>
             <div>
+                <p>At ProNotion we offer the most effective event planning online resource.</p>
+                <p>ProNotion offers great accesibility and functionality towards keeping tracks of projects, tasks and even reminders.</p>
+            </div>
+            <div className="feats">
+                <h2 className="headfeats">Features</h2>
+                <div>
+                    <div>Create <b><i>interactive and functional</i></b> tasks</div>
+                    <div>Coordinate your tasks with events and <b><i>calendar</i></b></div>
+                    <div>Reminders and <b><i>Enforcement</i></b></div>
+                    <div>Sort and rank projects and task to <b><i>Visualize</i></b> your productivity</div>
+                </div>
+            </div>
+
+            <div className="log-sign">
                 <Login/>
+                <Signup/>
             </div>
-          </div>
+            
+        </div>
         </>
     )
 }
